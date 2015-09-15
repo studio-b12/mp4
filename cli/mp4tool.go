c19c45eb26794f0bf871103a1ca87eae700caa55
Mihail Stoykov <M.Stoikov@gmail.com>
2015-09-15 16:43:33 +0300

    updating mp4tool.go and adding early returns, pprof and wait at the end of the command

--- cli/mp4tool.go
+++ cli/mp4tool.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	_ "net/http/pprof"
 	"os"
 	"time"
 
-	cli "github.com/jawher/mow.cli"
 	"github.com/MStoykov/mp4"
 	"github.com/MStoykov/mp4/filter"
+	cli "github.com/jawher/mow.cli"
 )
 
 func main() {
+	go func() {
+		fmt.Println(http.ListenAndServe(":6060", nil))
+	}()
+
 	cmd := cli.App("mp4tool", "MP4 command line tool")
 
 	cmd.Command("info", "Displays information about a media", func(cmd *cli.Cmd) {
@@ -35,18 +41,34 @@ func main() {
 			in, err := os.Open(*src)
 			if err != nil {
 				fmt.Println(err)
+				return
 			}
 			defer in.Close()
 			v, err := mp4.Decode(in)
 			if err != nil {
 				fmt.Println(err)
+				return
 			}
 			out, err := os.Create(*dst)
 			if err != nil {
 				fmt.Println(err)
+				return
 			}
 			defer out.Close()
-			filter.EncodeFiltered(out, v, filter.Clip(time.Duration(*start)*time.Second, time.Duration(*duration)*time.Second))
+			clip, err := filter.Clip(v, time.Duration(*start)*time.Second, time.Duration(*duration)*time.Second)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			if err := clip.Filter(); err != nil {
+				fmt.Println(err)
+				return
+			}
+			size, err := clip.WriteTo(out)
+			if err != nil {
+				fmt.Println(err)
+			}
+			fmt.Println("wrote", size)
 		}
 	})
 
@@ -72,4 +94,6 @@ func main() {
 		}
 	})
 	cmd.Run(os.Args)
+	fmt.Println("press return to exit the program")
+	fmt.Scanln()
 }
